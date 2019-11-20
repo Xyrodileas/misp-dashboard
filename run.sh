58b59d2ca41f861420d28ccf57343be7cb5e1235
Alexis Vuillaume <alexis.vuillaume@ubisoft.com>
2019-11-20 19:22:21 +0000

    init

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+env
+#Configure misp-dashboard
+sed -i "s#host=localhost#host=$REDISHOST#" config/config.cfg
+sed -i "s#port=6250#port=$REDISPORT#" config/config.cfg
+sed -i "s#misp_web_url = http://0.0.0.0#misp_web_url=$MISP_URL#" config/config.cfg
+
+cat config/config.cfg
+#Run misp-dashboard
+echo "Enabling virtualenv"
+. ./DASHENV/bin/activate
+echo "Starting zmq subscriber"
+./zmq_subscriber.py &
+echo "Starting zmq dispatcher"
+./zmq_dispatcher.py &
+echo "Starting server"
+./server.py
+
