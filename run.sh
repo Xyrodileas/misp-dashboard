fcaae6cc389e37f4765de70b44af38e8a74a4035
Alexis Vuillaume <alexis.vuillaume@ubisoft.com>
2019-11-21 14:52:52 +0000

    Adding binding to 0.0.0.0 instead of localhost

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 env
 #Configure misp-dashboard
+sed -i "s#host = localhost#host = 0.0.0.0/0#" config/config.cfg
 sed -i "s#host=localhost#host=$REDISHOST#" config/config.cfg
 sed -i "s#port=6250#port=$REDISPORT#" config/config.cfg
 sed -i "s#misp_web_url = http://0.0.0.0#misp_web_url=$MISP_URL#" config/config.cfg
@@ -15,4 +16,3 @@ echo "Starting zmq dispatcher"
 ./zmq_dispatcher.py &
 echo "Starting server"
 ./server.py
-
