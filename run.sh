5fc5cf6a2bf25482452ccc197e20e6503a8d168f
Alexis Vuillaume <alexis.vuillaume@ubisoft.com>
2019-11-21 15:07:35 +0000

    Remove /0

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 env
 #Configure misp-dashboard
-sed -i "s#host = localhost#host = 0.0.0.0/0#" config/config.cfg
+sed -i "s#host = localhost#host = 0.0.0.0#" config/config.cfg
 sed -i "s#host=localhost#host=$REDISHOST#" config/config.cfg
 sed -i "s#port=6250#port=$REDISPORT#" config/config.cfg
 sed -i "s#misp_web_url = http://0.0.0.0#misp_web_url=$MISP_URL#" config/config.cfg
