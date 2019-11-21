be138c423a7616a8e2559bac222394539765ed1f
Alexis Vuillaume <alexis.vuillaume@ubisoft.com>
2019-11-21 20:20:09 +0000

    Expose ZMQ configuration

--- run.sh
+++ run.sh
@@ -6,7 +6,11 @@ sed -i "s#host=localhost#host=$REDISHOST#" config/config.cfg
 sed -i "s#port=6250#port=$REDISPORT#" config/config.cfg
 sed -i "s#misp_web_url = http://0.0.0.0#misp_web_url=$MISP_URL#" config/config.cfg
 
+sed -i "s#\"url\": \"http://localhost\"#\"url\": \"$MISP_URL\"#" config/config.cfg
+sed -i "s#\"zmq\": \"tcp://localhost:50000\"#\"zmq\": \"tcp://$ZMQ_URL:$ZMQ_PORT\"#" config/config.cfg
+
 cat config/config.cfg
+
 #Run misp-dashboard
 echo "Enabling virtualenv"
 . ./DASHENV/bin/activate
